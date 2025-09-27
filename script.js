12bcca4b453a18f8e60011f1406d5fb533fe2982
Steven Sanchez <stevensanchez23@gmail.com>
2025-09-26 20:36:15 -0400

    Added for in loop and switch statement

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-const numGenerator = () => Math.floor(Math.random() * 4)
-
-
-console.log(numGenerator())
-
 const flights = {
     from: ['Atlanta', 'Dallas', 'Phoenix', 'Charlotte'],
     to: ['New York City', 'Los Angeles', 'Denver', 'Miami'],
     seat: ['First Class', 'Business', 'Premium Economy', 'Economy']
-}
+};
 
+/* 
 const flightGenerator = () => {
     const fromSelection = flights.from[numGenerator()];
     const toSelection = flights.to[numGenerator()];
@@ -16,5 +12,33 @@ const flightGenerator = () => {
 
     return `Your flight departs from ${fromSelection}! You're flying to ${toSelection}! Seat type: ${seatSelection}.`
 }
+*/
+
+const numGenerator = (num) => Math.floor(Math.random() * num);
+
+const flightGenerator = () => {
+    let flightInfo = [];
+
+    for (category in flights) {
+        const idx = numGenerator(flights[category].length);
+        const selection = flights[category][idx];
+        switch (category) {
+            case 'from':
+                flightInfo.push(`Your flight departs from ${selection}!`);
+                break;
+            case 'to':
+                flightInfo.push(`You're flying to ${selection}!`);
+                break;
+            case 'seat':
+                flightInfo.push(`Seat type: ${selection}`);
+                break;
+            default:
+                flightInfo.push(`Could not generate flight.`);   
+        }
+    }
+
+    return flightInfo.join('\n')
+}
+
 
 console.log(flightGenerator())
